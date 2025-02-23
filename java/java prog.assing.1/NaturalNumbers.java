456a980fb634d986d2709d3f42ea0bcbc03b79e0
Shilpa-u <Shilpapandey2309@gmail.com>
2025-02-23 19:41:05 +0530

    Add files via upload

---
+++ java/java prog.assing.1/NaturalNumbers.java
@@ -0,0 +1,9 @@
+public class NaturalNumbers{
+       public static void main(String[] args){
+       int no=6;
+       for(int i=1; i<=6; i++){
+           System.out.println(i);
+}
+}
+}
+        
---
+++ java/java prog.assing.1/Temperature.java
@@ -0,0 +1,10 @@
+public class Temperature{
+       public static void main(String[] args){
+       double celsius =25;
+       double fahrenheit=(celsius*9/5)+32;
+
+        System.out.println("The temperature in fahrenheit is: "+fahrenheit);
+}
+}
+
+        
