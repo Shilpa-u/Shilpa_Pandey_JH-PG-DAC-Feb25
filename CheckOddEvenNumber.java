c065abfae1d5c34d8d016abd0aacbbe30dbd77c8
Shilpa-u <Shilpapandey2309@gmail.com>
2025-02-23 19:15:41 +0530

    Add files via upload

---
+++ CheckOddEvenNumber.java
@@ -0,0 +1,12 @@
+public class CheckOddEvenNumber{
+    public static void main(String[] args){
+    int number=16;
+
+    if(number % 2 == 0){
+       System.out.println("The number is even");
+}
+    else{
+       System.out.println("The number is odd");
+}
+}
+}
---
+++ GoodMorningMessage.java
@@ -0,0 +1,16 @@
+public class GoodMorningMessage{
+    public static void main(String[] args){
+   
+     int time = 7;
+     if(time >= 5 && time <= 12){
+       System.out.println("Good Morning");
+}
+     else{
+      System.out.println("Not a Morning");
+}
+}
+}
+       
+
+
+    
