28faef0297f3738e95d5213198ccb0644111f367
Shilpa-u <Shilpapandey2309@gmail.com>
2025-02-23 19:43:23 +0530

    Add files via upload

---
+++ java/Assingment 1/CheckPositiveNumber.java
@@ -0,0 +1,12 @@
+public class CheckPositiveNumber{
+     public static void main(String[] args){
+      int number = 15;
+      
+if(number>0){
+   System.out.println("The number is positive");
+}
+else{
+   System.out.println("The number is negative");
+}
+}
+}
